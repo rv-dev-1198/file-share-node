9267eae8f97fe8338ecbb71e16dc29a38949f318
rahulvadhavani <66684896+rahulvadhavani@users.noreply.github.com>
2021-11-27 16:52:37 +0530

    change url

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
-app.use('/api/files',require('./routes/files')); 
+app.use('/',require('./routes/files')); 
 app.use('/files',require('./routes/show'));  
 
 app.listen(PORT,()=>{
