2f891fcb405cfa4acad12f218ded66fb0ee07bdb
rahulvadhavani <66684896+rahulvadhavani@users.noreply.github.com>
2021-11-27 18:05:16 +0530

    text changes

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,7 @@ const emailForm = document.querySelector("#emailForm");
 const toast = document.querySelector(".toast");
 
 // const baseURL = "http://localhost:3000";
-const baseURL = "http://localhost:https://file-share-rv.herokuapp.com";
+const baseURL = "https://file-share-rv.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send-mail`;
 
