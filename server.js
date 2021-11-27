28e22e1ebe2770410edd75641b2053b66b555a46
rahulvadhavani <66684896+rahulvadhavani@users.noreply.github.com>
2021-11-27 16:35:57 +0530

    first-commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+var bodyParser = require('body-parser');
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+const connectDB = require('./config/db');
+connectDB();
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use('/api/files',require('./routes/files')); 
+app.use('/files',require('./routes/show'));  
+
+app.listen(PORT,()=>{
+    console.log(`Listen on port ${PORT}`);
+})
